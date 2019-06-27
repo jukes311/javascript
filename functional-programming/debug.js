f38928ac5595807d4f920d483a8ad69ff307c011
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-06-27 10:17:38 -0400

    Added debug.js

---
+++ functional-programming/debug.js
@@ -0,0 +1,21 @@
+const DEBUG_MODE_ENABLED = true
+
+var debug
+
+if (DEBUG_MODE_ENABLED) {
+  debug = printDebugMessage
+} else {
+  debug = doNothing
+}
+
+// ...
+
+debug("Some debug message")
+
+// ...
+
+function printDebugMessage(message) {
+  console.log("DEBUG: " + message)
+}
+
+function doNothing() { }
