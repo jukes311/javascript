ecfb8e7762c3b640924869b979a7862936a67c97
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-06-28 11:08:06 -0400

    Updated with iife definition

--- misc/iifeExample.js
+++ misc/iifeExample.js
@@ -1,3 +1,15 @@
+/*
+Immediately Invoked Function Expression: 
+
+Advantages of IIFE:
+
+Do not create unnecessary global variables and functions
+Functions and variables defined in IIFE do not conflict with other functions & variables even if they have same name.
+Organize JavaScript code.
+Make JavaScript code maintainable.
+
+*/
+
 var userName = "Bill";
 
 (function (name) {
