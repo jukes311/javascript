2dfe1766c3abb2a17fb1d3cc5c7ef69f70cddeae
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-06-28 11:27:15 -0400

    Const example

---
+++ typescript/constExample.ts
@@ -0,0 +1,21 @@
+/**
+ * Const Example
+ * 
+ * Const variables allow an object sub-properties to be changed but not the object structure.
+ */
+
+
+const playerCodes = { 
+    player1 : 9, 
+    player2 : 10, 
+    player3 : 13, 
+    player4 : 20
+}; 
+playerCodes.player2 = 11; // OK
+
+playerCodes = {     //Compiler Error: Cannot assign to playerCodes because it is a constant or read-only
+    player1 : 50,   // Modified value
+    player2 : 10, 
+    player3 : 13, 
+    player4 : 20
+}; 
