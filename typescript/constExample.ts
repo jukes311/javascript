ec92a0bbd9fa6edb37ec88b475c85ca1682f597a
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-06-28 16:23:55 -0400

    commented code that will create a compilation error

--- typescript/constExample.ts
+++ typescript/constExample.ts
@@ -13,9 +13,9 @@ const playerCodes = {
 }; 
 playerCodes.player2 = 11; // OK
 
-playerCodes = {     //Compiler Error: Cannot assign to playerCodes because it is a constant or read-only
-    player1 : 50,   // Modified value
-    player2 : 10, 
-    player3 : 13, 
-    player4 : 20
-}; 
+// playerCodes = {     //Compiler Error: Cannot assign to playerCodes because it is a constant or read-only
+//     player1 : 50,   // Modified value
+//     player2 : 10, 
+//     player3 : 13, 
+//     player4 : 20
+// }; 
