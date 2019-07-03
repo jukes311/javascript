ea75d24636f8c6c82b07e5e62feb2e14369441e5
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-07-02 22:41:03 -0400

    Adding components

--- react/react2019/my-app/src/App.js
+++ react/react2019/my-app/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
     );
   }
 }
---
+++ react/react2019/my-app/src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MainContent extends React.Component {
+    render() {
+        return(
+            <p>This is a Main Content.</p>
+        )
+    }
+}
+
+export default MainContent
