43fc0fdf9b2814c165fc9973b47ec3f6d9ece49e
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-07-09 19:23:39 -0400

    Forms Updated

--- react/react2019/my-app/src/App.js
+++ react/react2019/my-app/src/App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
     super()
     this.state = {
       firstName: '',
-      lastName: ''
+      lastName: '',
+      isFriendly: true
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -24,6 +25,10 @@ class App extends React.Component {
         <input type="text" name="firstName" value={this.state.firstName} placeholder="First Name" onChange={this.handleChange} />
         <br /> 
         <input type="text" name="lastName" value={this.state.lastName} placeholder="Last Name" onChange={this.handleChange} />
+        <br />
+        <textarea value="some value" />
+        <br />
+        <input type="checkbox" name="isFriendly" checked={this.state.isFriendly} onChange={this.handleChange} /> Is Friendly?
         <h1>{this.state.firstName} {this.state.lastName}</h1>
       </div>
     )
