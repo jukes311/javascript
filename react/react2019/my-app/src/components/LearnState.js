bdedbece3f16462dde41a215b460e96bc8cd3056
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-07-09 15:13:49 -0400

    Add onClick event.

--- react/react2019/my-app/src/components/LearnState.js
+++ react/react2019/my-app/src/components/LearnState.js
@@ -4,17 +4,23 @@ class LearnState extends React.Component {
     constructor() {
         super()
         this.state = {
-            name: 'Name state from LearnState.js'
+            name: 'Name state from LearnState.js',
+            count: 0
         }
 
+    }
+
+    handleClick() {
+        console.log('clicked.')
     }
 
     render() {
         return(
             //JSX
             <div>
+                <h1>{this.state.count}</h1>
                 {this.state.name}
-
+                <button onClick={this.handleClick}>Increment Count</button>
             </div>
         )
     }
