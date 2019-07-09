813f0bb3cefe4185bdd19b85fe715afb238d6cf1
Christian Saladores <christiansaladores@Christians-MacBook-Pro.local>
2019-07-09 19:44:34 -0400

    Forms part 2: checkbox, radio, text area, select.

--- react/react2019/my-app/src/App.js
+++ react/react2019/my-app/src/App.js
@@ -7,30 +7,48 @@ class App extends React.Component {
     this.state = {
       firstName: '',
       lastName: '',
-      isFriendly: true
+      isFriendly: true,
+      gender: '',
+      favColor: 'blue'
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event){
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+    const {name, value, type, checked} = event.target
+    type === "checkbox" ? this.setState( {[name]: checked} ) : this.setState( {[name]: value} )
 
   }
 
   render() {
     return (
-      <div>
+      <form onSubmit="">
         <input type="text" name="firstName" value={this.state.firstName} placeholder="First Name" onChange={this.handleChange} />
         <br /> 
         <input type="text" name="lastName" value={this.state.lastName} placeholder="Last Name" onChange={this.handleChange} />
         <br />
-        <textarea value="some value" />
+        <textarea value="some value" onChange={this.handleChange} />
         <br />
         <input type="checkbox" name="isFriendly" checked={this.state.isFriendly} onChange={this.handleChange} /> Is Friendly?
+        <br />
+        <input type="radio" name="gender" value="male" checked={this.state.gender === "male"} onChange={this.handleChange} /> Male 
+        <br />
+        <input type="radio" name="gender" value="female" checked={this.state.gender === "female"} onChange={this.handleChange} /> Female 
+        <br />
+        <select 
+          value={this.state.favColor}
+          onChange={this.handleChange}
+          name="favColor"
+        >
+          <option value="blue">Blue</option>
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+        </select>
+
+        <br />
+        <button>Submit</button>
         <h1>{this.state.firstName} {this.state.lastName}</h1>
-      </div>
+      </form>
     )
   }
 }
